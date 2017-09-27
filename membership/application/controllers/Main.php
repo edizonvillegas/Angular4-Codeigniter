226cec6d7274ea73b79df4ba72a439514f75b6f7
Edizon <edizon.villegas@transcosmos.com.ph>
2017-09-27 13:26:50 +0800

    added codeigniter files and sql

---
+++ membership/application/controllers/Main.php
@@ -0,0 +1,70 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Main extends CI_Controller {
+
+	function __construct() {
+		parent:: __construct();
+		header('Access-Control-Allow-Origin: *');
+        header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+        header('Access-Control-Allow-Methods: GET, POST, PUT');
+		$this->load->model('Users_model');
+	}
+
+	function getUsers() {
+		$getUsers = $this->Users_model->getUsers();
+		print_r($getUsers);
+	}
+
+	function getUserById($id) {
+		$getUserById =  $this->Users_model->getUserById($id);
+		print_r($getUserById);
+	}
+
+	function getUserById2($id) {
+		$getUserById2 =  $this->Users_model->getUserById2($id);
+		print_r($getUserById2);
+	}
+
+	function editUser() {
+		$postdata = file_get_contents("php://input");
+		$request = json_decode($postdata);
+		if ($request)
+		$editUser = $this->Users_model->editUser($request, $request->idUser);
+	}
+
+	function addUser() {
+		$postdata = file_get_contents("php://input");
+		$request = json_decode($postdata);
+		if ($request)
+		$addUser = $this->Users_model->addUser($request);
+	}
+
+	function deleteUser() {
+		$postdata = file_get_contents("php://input");
+		$request = json_decode($postdata);
+		if ($request)
+		$this->Users_model->deleteUser($request);
+	}
+
+	function login() {
+		$postdata = file_get_contents("php://input");
+		$request = json_decode($postdata);
+
+		$username = $request->acct_username;
+		$password = $request->acct_password;
+
+		$login = $this->Users_model->login($username);
+		if ($login) {
+			if (password_verify($password, $login->acct_password) ) {
+				echo "success";
+			}
+		} else {
+			echo "invalid user";
+		}
+	}
+
+	function searchMember($str) {
+		$res = $this->Users_model->searchMember($str);
+		print_r($res);
+	}
+}
---
+++ membership/application/models/Users_model.php
@@ -0,0 +1,114 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Users_model extends CI_Model {
+
+	public $usersTable = 'mem_profile';
+	public $accountsTable = 'mem_accounts';
+
+	function getUsers() {
+		$getUsers = $this->db->select("
+			user_id AS id,
+			CONCAT(user_firstname, ' ', user_lastname) AS fullName,
+			DATE_FORMAT(user_date_added, '%M %d %Y %H:%i:%s') AS dateAdded,
+			user_status AS status
+		")
+		->where('user_status' , 1)
+		->from($this->usersTable)
+		->get()
+		->result();
+		foreach($getUsers as $users) {
+			$rows[] = $users;
+		}
+		echo json_encode($rows);
+	}
+
+	function getUserById($id) {
+		$getUserById = $this->db
+			->select("
+				user_id AS id,
+				CONCAT(user_firstname, ' ', user_lastname) AS fullName,
+				DATE_FORMAT(user_date_added, '%M %d %Y %H:%i:%s') AS dateAdded,
+				user_status AS status
+			")
+			->where('user_id', $id)
+			->from($this->usersTable)
+			->get()
+			->row();
+		$rows = $getUserById;
+		echo json_encode($rows);
+	}
+
+	function searchMember($str) {
+		$getUsers = $this->db->select("
+			user_id AS id,
+			CONCAT(user_firstname, ' ', user_lastname) AS fullName,
+			DATE_FORMAT(user_date_added, '%M %d %Y %H:%i:%s') AS dateAdded,
+			user_status AS status
+		")
+		->like('user_lastname' , $str)
+		->from($this->usersTable)
+		->get()
+		->result();
+		foreach($getUsers as $users) {
+			$rows[] = $users;
+		}
+		echo json_encode($rows);
+	}
+
+	function getUserById2($id) {
+		$getUserById2 = $this->db
+			->select("
+				user_id AS id,
+				user_firstname AS firstname,
+				user_lastname AS lastname,
+				DATE_FORMAT(user_date_added, '%M %d %Y %H:%i:%s') AS dateAdded,
+				user_status AS status
+			")
+			->where('user_id', $id)
+			->from($this->usersTable)
+			->get()
+			->row();
+		$rows = $getUserById2;
+		echo json_encode($rows);
+	}
+
+	function editUser($formData = array()) {
+		$data = [
+			'user_firstname' => $formData->user_firstname,
+			'user_lastname' => $formData->user_lastname
+		];
+		$where = [
+			'user_id' => $formData->idUser
+		];
+		$getId = $this->db->update($this->usersTable, $data, $where);
+		return $getId;
+	}
+
+	function addUser($formData = array() ) {
+		$data = [
+			'user_firstname' => $formData->user_firstname,
+			'user_lastname' => $formData->user_lastname,
+			'user_status' => 1
+		];
+		$addUser = $this->db->insert($this->usersTable, $data);
+	}
+
+	function deleteUser($id) {
+		$data = [
+			'user_status' => 0
+		];
+		$where = [
+			'user_id' => $id
+		];
+		$deleteUser = $this->db->update($this->usersTable, $data, $where);
+		return $deleteUser;
+	}
+
+	function login($username) {
+		$login = $this->db
+			->where('acct_username', $username)
+			->get($this->accountsTable)
+			->row();
+		return $login;
+	}
+}
